4b9a851c373c22fc8df3f8575d69917c46f63715
Aaron Wang <aaron.wang@duolingo.com>
2022-11-27 10:03:54 -0800

    Add instructions and code cleanup

--- visa.py
+++ visa.py
@@ -1,12 +1,13 @@
 # -*- coding: utf8 -*-
 
-# Source: https://gist.github.com/Svision/04202d93fb32d14f00ac746879820722
+# Original script: https://gist.github.com/Svision/04202d93fb32d14f00ac746879820722
+
 import time
 import json
 import random
-import platform
 from datetime import datetime
 from os import system
+import argparse
 
 import requests
 from selenium import webdriver
@@ -16,32 +17,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-USERNAME = '<username>'
-PASSWORD = '<password>'
-SCHEDULE = '37089586'
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-u', '--username', type=str, required=True, help='Username')
+# Note: It's a security risk to pass in password this way as anyone who can run `ps` will see the password, but w/e...
+parser.add_argument('-p', '--password', type=str, required=True, help='Password')
+parser.add_argument('-s', '--schedule', type=int, required=True, help='8-digit schedule name.')
+parser.add_argument('-d', '--date', required=False, default='2023-03-07',
+                    help='Date in YYYY-MM-DD format. Only notify if appointments earlier than this is found.')
 
-PUSH_TOKEN = '<my push token>'
-PUSH_USER = '<my push user>'
+args = parser.parse_args()
 
-MY_SCHEDULE_DATE = "2023-03-07" # 2020-12-02
-MY_CONDITION = lambda month,day: int(month) == 11 and int(day) >= 5
 
-SLEEP_TIME = 180   # recheck time interval
+QUERY_INTERVAL_IN_SEC = 180   # recheck time interval
 
 DATE_URL_TMPL = "https://ais.usvisa-info.com/en-ca/niv/schedule/%s/appointment/days/%d.json?appointments[expedite]=false"
 LOCATION_MAP = {89: 'calgary', 90: 'helifax', 91: 'montreal', 92: 'ottawa', 93: 'quebec city', 94: 'toronto', 95: 'vancouver'}
 
-# TIME_URL = "https://ais.usvisa-info.com/en-ca/niv/schedule/%s/appointment/times/95.json?date=%%s&appointments[expedite]=false" % SCHEDULE
-# APPOINTMENT_URL = "https://ais.usvisa-info.com/en-ca/niv/schedule/%s/appointment" % SCHEDULE
-
-def send(msg):
-    url = "https://api.pushover.net/1/messages.json"
-    data = {
-        "token": PUSH_TOKEN,
-        "user": PUSH_USER,
-        "message": msg
-    }
-    requests.post(url, data)
 
 
 def get_drive():
@@ -59,11 +50,10 @@ def login():
     a.click()
     time.sleep(1)
 
-    print("start sign")
+    print("start signing in")
     href = driver.find_element(By.XPATH, '//*[@id="header"]/nav/div[2]/div[1]/ul/li[3]/a')
     href.click()
-    # time.sleep(5)
-    # print(driver.get_cookie("_yatri_session")["value"])
+    # print("DEBUG: " + driver.get_cookie("_yatri_session")["value"])
     Wait(driver, 60).until(EC.presence_of_element_located((By.NAME, "commit")))
 
     print("click bounce")
@@ -77,12 +67,12 @@ def login():
 def do_login_action():
     print("input email")
     user = driver.find_element(By.ID, 'user_email')
-    user.send_keys(USERNAME)
+    user.send_keys(args.username)
     time.sleep(random.randint(1, 3))
 
     print("input pwd")
     pw = driver.find_element(By.ID, 'user_password')
-    pw.send_keys(PASSWORD) 
+    pw.send_keys(args.password) 
     time.sleep(random.randint(1, 3))
 
     print("click privacy")
@@ -95,15 +85,14 @@ def do_login_action():
     btn.click()
     time.sleep(random.randint(1, 3))
 
-    # time.sleep(5)
-    # print(driver.get_cookie("_yatri_session")["value"])
+    # print("DEBUG: " + driver.get_cookie("_yatri_session")["value"])
 
     Wait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Continue')]")))
     print("Login successfully! ")
 
 
 def get_date(location):
-    driver.get(DATE_URL_TMPL % (SCHEDULE, location))
+    driver.get(DATE_URL_TMPL % (args.schedule, location))
     if not is_logined():
         login()
         return get_date(location)
@@ -127,27 +116,33 @@ def print_date(dates):
 
 def get_available_date(dates):
     def is_earlier(date):
-        return datetime.strptime(MY_SCHEDULE_DATE, "%Y-%m-%d") > datetime.strptime(date, "%Y-%m-%d")
+        return datetime.strptime(args.date, "%Y-%m-%d") > datetime.strptime(date, "%Y-%m-%d")
 
     for d in dates:
         date = d.get('date')
         if is_earlier(date):
-            # year, month, day = date.split('-')
-            # if(MY_CONDITION(month, day)):
-            #     last_seen = date
             return date
 
 
 def push_notification(dates):
+    PUSHOVER_TOKEN = '<my push token>'
+    PUSHOVER_USER = '<my push user>'
     msg = "date: "
     for d in dates:
         msg = msg + d.get('date') + '; '
-    send(msg)
+    url = "https://api.pushover.net/1/messages.json"
+    data = {
+        "token": PUSHOVER_TOKEN,
+        "user": PUSHOVER_USER,
+        "message": msg
+    }
+    requests.post(url, data)
 
 
 if __name__ == "__main__":
     login()
     retry_count = 0
+    start_loc, end_loc = min(LOCATION_MAP.keys()), max(LOCATION_MAP.keys())
     while 1:
         if retry_count > 6:
             break
@@ -155,15 +150,18 @@ if __name__ == "__main__":
             print()
             print(datetime.today())
             print("------------------")
-            for location in range(89,96):
+            for location in range(start_loc, end_loc):
                 dates = get_date(location)[:5]
+                print(f'{LOCATION_MAP[location]} has dates: {dates}')
                 date = get_available_date(dates)
                 if date:
+                    # TODO: `say` is a command in Mac, feel free to replace with other way of doing notification.
                     system('say Date {} is available at {}'.format(date[0], LOCATION_MAP[location]))
-                    print('Location: ', location, LOCATION_MAP[location], '; \tGood dates: ', date)
+                    print(f'Location: {location} - {LOCATION_MAP[location]} \t Good dates {date}')
                     print_date(dates)
+                    push_notification(date)
 
-            time.sleep(SLEEP_TIME)
+            time.sleep(QUERY_INTERVAL_IN_SEC)
         except:
             retry_count += 1
             time.sleep(60)
